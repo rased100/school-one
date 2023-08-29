fb13ed266a018e96192ebc3dcbd4fd69de827470
Md Rasedul Islam <rased100@gmail.com>
2023-08-30 00:05:28 +0600

    add tailwind css

---
+++ src/pages/nav/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold bg-gray-500">navbar page</h2>
+    </div>
+  );
+};
+
+export default Navbar;
