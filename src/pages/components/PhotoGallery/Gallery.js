9a773f66a55d0df9513c64b4cac6d8d3683c984f
Md Rasedul Islam <rased100@gmail.com>
2023-09-05 12:22:16 +0600

    done

--- src/pages/components/PhotoGallery/Gallery.js
+++ src/pages/components/PhotoGallery/Gallery.js
@@ -3,6 +3,11 @@ import GalleryCard from "./GalleryCard";
 import img1 from "../../../assets/cImg1.jpg";
 import img2 from "../../../assets/cImg2.jpg";
 import img3 from "../../../assets/cImg3.jpg";
+import img4 from "../../../assets/cImg4.jpg";
+import img5 from "../../../assets/21Feb.jpg";
+import img6 from "../../../assets/21Feb2.jpg";
+import img7 from "../../../assets/21Feb3.jpg";
+import img8 from "../../../assets/groupimg1.jpg";
 
 const Gallery = () => {
   const images = [
@@ -17,6 +22,30 @@ const Gallery = () => {
     {
       id: "03",
       img: img3,
+    },
+    {
+      id: "04",
+      img: img4,
+    },
+    {
+      id: "05",
+      img: img5,
+    },
+    {
+      id: "06",
+      img: img6,
+    },
+    {
+      id: "07",
+      img: img7,
+    },
+    {
+      id: "08",
+      img: img8,
+    },
+    {
+      id: "04",
+      img: img4,
     },
   ];
   return (
