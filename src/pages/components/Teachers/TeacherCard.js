c7c23b3bb78621885080a84775e24bbac89a1222
Md Rasedul Islam <rased100@gmail.com>
2023-09-13 20:46:07 +0600

    teachers

--- src/pages/components/Teachers/TeacherCard.js
+++ src/pages/components/Teachers/TeacherCard.js
@@ -2,27 +2,20 @@ import React from "react";
 
 const TeacherCard = ({ img, name, designation, desc1, desc2, desc3 }) => {
   return (
-    <div
-      // data-aos="zoom-out"
-      // data-aos-easing="ease-in-out-back"
-      // data-aos-duration="1000"
-      //
-      className="w-full bg-white p-3 rounded-xl shadow-xl"
-    >
-      <div className="flex justify-around items-center rounded-full">
+    <div className="w-full bg-white p-3 rounded-xl shadow-xl">
+      <div className="flex justify-around items-center rounded-full grid grid-cols-1 md:grid-cols-2 gap-2">
         <div
           data-aos="zoom-in"
           data-aos-duration="1500"
           //
+          className="md:col-span-1 flex items-center justify-center"
         >
           <img src={img} alt="" className=" rounded-full w-[200px]" />
         </div>
-        <div>
-          <p className="font-bold text-left">Name: {name}</p>
-          <p className="font-bold text-left">{designation}</p>
-          {/* <p className="font-bold text-left">{desc1}</p> */}
-          <p className=" text-left text-blue-500">Edicational info</p>
-          <p className=" text-left">{desc3}</p>
+        <div className="py-5 px-5 md:col-span-1">
+          <p className="font-bold text-center">{name}</p>
+          <p className="text-center">{designation}</p>
+          <p className="text-center">{desc3}</p>
         </div>
       </div>
     </div>
