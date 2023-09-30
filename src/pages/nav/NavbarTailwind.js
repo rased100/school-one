aa48a483308bab9e0da619a6e3d295814d94fd4e
kishalayabiddaniketan <kishalayabiddaniketan1994@gmail.com>
2023-10-01 01:47:05 +0600

    nav nar 2

--- src/pages/nav/NavbarTailwind.js
+++ src/pages/nav/NavbarTailwind.js
@@ -25,16 +25,16 @@ const NavbarTailwind = () => {
           About us
         </Link>,
         <Link to="" key="SubLink1">
-          SubLink 1.2
+          Hisroey of Institute
         </Link>,
         <Link to="" key="SubLink2">
-          SubLink 1.3
+          Mission & Vision
         </Link>,
         <Link to="" key="SubLink3">
-          SubLink 1.4
+          Achievements
         </Link>,
         <Link to="" key="SubLink4">
-          SubLink 1.5
+          News & Events
         </Link>,
       ],
     },
@@ -42,19 +42,19 @@ const NavbarTailwind = () => {
       label: "Administration",
       links: [
         <Link to="" key="SubLink2.1">
-          SubLink 2.1
+          Governing Body
         </Link>,
         <Link to="" key="SubLink2.2">
-          SubLink 2.2
+          Chief Patron's Message
         </Link>,
         <Link to="" key="SubLink2.3">
-          SubLink 2.3
+          Principal's Message
         </Link>,
         <Link to="" key="SubLink2.4">
-          SubLink 2.4
+          Teacher
         </Link>,
         <Link to="" key="SubLink2.5">
-          SubLink 2.5
+          Staff
         </Link>,
       ],
     },
@@ -62,19 +62,19 @@ const NavbarTailwind = () => {
       label: "Academic",
       links: [
         <Link to="" key="SubLink3.1">
-          SubLink 3.1
+          Class Routine
         </Link>,
         <Link to="" key="SubLink3.2">
-          SubLink 3.2
+          Exam Routine
         </Link>,
         <Link to="" key="SubLink3.3">
-          SubLink 3.3
+          Notice
         </Link>,
         <Link to="" key="SubLink3.4">
-          SubLink 3.4
+          Syllabus
         </Link>,
         <Link to="" key="SubLink3.5">
-          SubLink 3.5
+          Calendar & Year Planner
         </Link>,
       ],
     },
@@ -82,19 +82,19 @@ const NavbarTailwind = () => {
       label: "Admission",
       links: [
         <Link to="" key="SubLink4.1">
-          SubLink 4.1
+          Admission Circular
         </Link>,
         <Link to="" key="SubLink4.2">
-          SubLink 4.2
+          Prospectus
         </Link>,
         <Link to="" key="SubLink4.3">
-          SubLink 4.3
+          Admission Result
         </Link>,
         <Link to="" key="SubLink4.4">
-          SubLink 4.4
+          Admission Form
         </Link>,
         <Link to="" key="SubLink4.5">
-          SubLink 4.5
+          Selected Candidates
         </Link>,
       ],
     },
@@ -102,79 +102,70 @@ const NavbarTailwind = () => {
       label: "Result",
       links: [
         <Link to="" key="SubLink5.1">
-          SubLink 5.1
+          PSC Result
         </Link>,
         <Link to="" key="SubLink5.2">
-          SubLink 5.2
+          JSC Result
         </Link>,
         <Link to="" key="SubLink5.3">
-          SubLink 5.3
-        </Link>,
-        <Link to="" key="SubLink5.4">
-          SubLink 5.4
-        </Link>,
-        <Link to="" key="SubLink5.5">
-          SubLink 5.5
+          SSC Result
         </Link>,
       ],
     },
-    {
-      label: "Co-Curricular",
-      links: [
-        <Link to="" key="SubLink6.1">
-          SubLink 6.1
-        </Link>,
-        <Link to="" key="SubLink6.2">
-          SubLink 6.2
-        </Link>,
-        <Link to="" key="SubLink6.3">
-          SubLink 6.3
-        </Link>,
-        <Link to="" key="SubLink6.4">
-          SubLink 6.4
-        </Link>,
-        <Link to="" key="SubLink6.5">
-          SubLink 6.5
-        </Link>,
-      ],
-    },
-    {
-      label: "Facilities",
-      links: [
-        <Link to="" key="SubLink7.1">
-          SubLink 7.1
-        </Link>,
-        <Link to="" key="SubLink7.2">
-          SubLink 7.2
-        </Link>,
-        <Link to="" key="SubLink7.3">
-          SubLink 7.3
-        </Link>,
-        <Link to="" key="SubLink7.4">
-          SubLink 7.4
-        </Link>,
-        <Link to="" key="SubLink7.5">
-          SubLink 7.5
-        </Link>,
-      ],
-    },
+    // {
+    //   label: "Co-Curricular",
+    //   links: [
+    //     <Link to="" key="SubLink6.1">
+    //       SubLink 6.1
+    //     </Link>,
+    //     <Link to="" key="SubLink6.2">
+    //       SubLink 6.2
+    //     </Link>,
+    //     <Link to="" key="SubLink6.3">
+    //       SubLink 6.3
+    //     </Link>,
+    //     <Link to="" key="SubLink6.4">
+    //       SubLink 6.4
+    //     </Link>,
+    //     <Link to="" key="SubLink6.5">
+    //       SubLink 6.5
+    //     </Link>,
+    //   ],
+    // },
+    // {
+    //   label: "Facilities",
+    //   links: [
+    //     <Link to="" key="SubLink7.1">
+    //       SubLink 7.1
+    //     </Link>,
+    //     <Link to="" key="SubLink7.2">
+    //       SubLink 7.2
+    //     </Link>,
+    //     <Link to="" key="SubLink7.3">
+    //       SubLink 7.3
+    //     </Link>,
+    //     <Link to="" key="SubLink7.4">
+    //       SubLink 7.4
+    //     </Link>,
+    //     <Link to="" key="SubLink7.5">
+    //       SubLink 7.5
+    //     </Link>,
+    //   ],
+    // },
     {
       label: "Library",
       links: [
         <Link to="" key="SubLink8.1">
-          SubLink 8.1
+          Debating Club
         </Link>,
         <Link to="" key="SubLink8.2">
-          SubLink 8.2
+          Readers & Writers Club
         </Link>,
         <Link to="" key="SubLink8.3">
-          SubLink 8.3
+          Extempore Speech Club
         </Link>,
         <Link to="" key="SubLink8.4">
-          SubLink 8.4
-        </Link>,
-        <Link to="" key="SubLink8.5">
-          SubLink 8.5
+          Presentation Club
         </Link>,
       ],
     },
@@ -196,7 +187,7 @@ const NavbarTailwind = () => {
   };
 
   return (
-    <div className="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 ">
+    <div className="text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 ">
       <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <nav className="flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-center md:flex-row">
           {dropdownItems.map((item, index) => (
@@ -207,7 +198,7 @@ const NavbarTailwind = () => {
               onMouseLeave={() => toggleDropdown(null)}
             >
               <button
-                className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-white focus:text-gray-900  focus:bg-gray-200 focus:outline-none focus:shadow-outline hover:bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700"
+                className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-white focus:text-gray-900  focus:bg-gray-200 focus:outline-none focus:shadow-outline hover:bg-gradient-to-r from-rose-600 via-purple-700 to-purple-700"
 
                 // className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-4 py-1.5 text-center"
               >
@@ -228,7 +219,7 @@ const NavbarTailwind = () => {
               </button>
               {openDropdown === index && (
                 <div
-                  className="absolute right-0 mt-0 origin-top-right rounded-md shadow-lg md:w-48"
+                  className="absolute right-0 md:left-0  origin-top-right rounded-md shadow-lg md:w-48"
                   style={{ zIndex: 1000 }}
                 >
                   <div className="px-2 py-2  rounded-md shadow dark-mode:bg-gray-800 bg-gradient-to-r from-rose-600 via-purple-700 to-purple-700">
