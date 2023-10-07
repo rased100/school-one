f76eb25b8bf511badcf1469ce56afba3fa9f065f
kishalayabiddaniketan <kishalayabiddaniketan1994@gmail.com>
2023-10-07 12:03:26 +0600

    notice responsive

--- src/pages/NoticeBoard/NoticeBoard.js
+++ src/pages/NoticeBoard/NoticeBoard.js
@@ -4,14 +4,14 @@ import NoticeLink from "../components/NoticeLink";
 
 const NoticeBoard = () => {
   return (
-    <div className="max-h-screen bg-gray-700 px-10 py-10 scroll-auto">
+    <div className=" bg-gray-700 px-10 py-10 scroll-auto">
       {/* <h2 className="text-center text-4xl font-extrabold">Notice Board</h2> */}
-      <div className="flex">
+      <div className="lg:flex h-[100vh] lg:h-[50vh]  ">
         <div className=" flex justify-center items-center">
           <img src={hTeacher} className="h-[50vh]  rounded-lg shadow-2xl" />
         </div>
-        <div className="flex-1 justify-center items-center rounded-lg pl-10">
-          <div className="h-[50vh] flex-1 overflow-y-auto">
+        <div className="flex-1 justify-center items-center rounded-lg lg:pl-10  py-10 lg:py-0">
+          <div className="h-[50vh] flex-1 overflow-y-auto overflow-hidden">
             <NoticeLink name="Examination Routine" />
             <NoticeLink name="Class Routine" />
             <NoticeLink name="Examination Routine" />
